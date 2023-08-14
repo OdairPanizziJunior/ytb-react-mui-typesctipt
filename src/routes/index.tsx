d32a7fec3a0311cf9a89a37458a5e6ccd9a1c6a8
Odair Panizzi Júnior <101676143+OdairPanizziJunior@users.noreply.github.com>
2023-08-14 08:49:28 -0300

    feat:
    Adicionado Router Dom

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,3 +1,11 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
+
 export const AppRoutes = () => {
-    return
+    return (
+        <Routes>
+            <Route path="/pagina-inicial" element={<p>Página Inicial</p>} />
+
+            <Route path="*" element={<Navigate to="/pagina-inicial" />} />
+        </Routes>
+    );
 };
