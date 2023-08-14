d236698862a30be31f5be9aab4565e5ed16eb3a0
Odair Panizzi Júnior <101676143+OdairPanizziJunior@users.noreply.github.com>
2023-08-13 21:38:17 -0300

    Updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { BrowserRouter } from "react-router-dom";
+
 export const App = () => {
   return (
+    <BrowserRouter>
     <div className="App">
       Teste!
     </div>
+    </BrowserRouter>
   );
 }
