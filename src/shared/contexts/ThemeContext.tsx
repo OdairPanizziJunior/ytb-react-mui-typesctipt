62f55f97693f068ef9b2a1e357179a092619925b
Odair Panizzi Júnior <101676143+OdairPanizziJunior@users.noreply.github.com>
2023-08-14 12:11:34 -0300

    feat:
    Adicionado Troca de tema

---
+++ src/shared/contexts/ThemeContext.tsx
@@ -0,0 +1,45 @@
+import { createContext, useCallback, useContext, useMemo, useState } from 'react';
+import { ThemeProvider } from '@mui/material';
+import { DarkTheme, LightTheme } from './../themes';
+import { Box } from '@mui/system';
+
+interface IThemeContextData {
+    themeName: 'Light' | 'Dark';
+    toogleTheme: () => void;
+}
+
+const ThemeContext = createContext({} as IThemeContextData);
+
+export const useAppThemeContext = () =>{
+    return useContext(ThemeContext);
+}
+
+interface IAppThemeProviderProps {
+    children:React.ReactNode;
+}
+
+export const AppThemeProvider: React.FC<IAppThemeProviderProps> = ({ children }) => {
+    const [themeName, setThemeName] = useState<'Light' | 'Dark'>('Light');
+
+    const toogleTheme = useCallback(() => {
+        setThemeName(oldThemeName => oldThemeName === 'Light' ? 'Dark' : 'Light');
+    }, []);
+
+    const theme = useMemo(() => {
+        if (themeName == 'Light') return LightTheme;
+
+        return DarkTheme;
+    }, [themeName]);
+
+
+    return (
+
+        <ThemeContext.Provider value={{ themeName, toogleTheme }}>
+            <ThemeProvider theme={theme}>
+                <Box width="100vw" height="100vh" bgcolor={theme.palette.background.default}>
+                    {children}
+                </Box>
+            </ThemeProvider>
+        </ThemeContext.Provider>
+    );
+}
